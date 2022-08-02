fb7d2d7dc27b640d8033bc66c25531019f96285d
Jiangli531 <409516522@qq.com>
2022-08-02 15:37:47 +0800

    url修正

--- DocsEdit/urls.py
+++ DocsEdit/urls.py
@@ -5,14 +5,14 @@ from .views import *
 urlpatterns = [
     # path('url_name', api_name)
     # 这是一个样例，指定路由名为url_name，对应处理函数为当前app内views.py中的api_name
-    path('viewDocList', viewDocList),
-    path('createDocument', createDocument),
-    path('viewDoc', viewDoc),
-    path('modifyDocName', modifyDocName),
-    path('modifyDocContent', modifyDocContent),
-    path('recycleDoc', recycleDoc),
-    path('delRecycleDoc', delRecycleDoc),
-    path('recover', recover),
+    path('viewDocList/', viewDocList),
+    path('createDocument/', createDocument),
+    path('viewDoc/', viewDoc),
+    path('modifyDocName/', modifyDocName),
+    path('modifyDocContent/', modifyDocContent),
+    path('recycleDoc/', recycleDoc),
+    path('delRecycleDoc/', delRecycleDoc),
+    path('recover/', recover),
 
 
 ]
