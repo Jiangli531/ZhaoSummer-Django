b4e20609b7884dcbf7138862874b5d04a042fde0
Jiangli531 <409516522@qq.com>
2022-08-02 16:54:07 +0800

    完成了团队邀请时的邮件美化

--- utils/send.py
+++ utils/send.py
@@ -1,5 +1,10 @@
+import smtplib
+from email.mime.text import MIMEText
+from email.utils import formataddr
+
 from django.conf import settings
 from Login.models import *
+from ZhaoSummer_Django.settings import EMAIL_FROM, EMAIL_HOST_USER, EMAIL_HOST_PASSWORD
 
 from utils.hash import *
 
@@ -12,6 +17,24 @@ def make_confirm_string(user):  # generate confirm_code for user (username+c_tim
     code = hash_code(user.username, now)
     ConfirmString.objects.create(code=code, user=user)
     return code
+
+
+def send_email_note(to, email_body, email_title):
+    try:
+        msg = MIMEText(email_body, _subtype='html', _charset='utf-8')  # 发送html格式
+        # msg=MIMEText(data, _charset='utf-8') # data 发送文本
+        msg['From'] = formataddr(["发送者名称", EMAIL_FROM])  # 括号里的对应发件人邮箱昵称、发件人邮箱账号
+        msg['To'] = formataddr(["接收者名称", to])  # 括号里的对应收件人邮箱昵称、收件人邮箱账号
+        msg['Subject'] = email_title  # 邮件的主题，也可以说是标题
+        server = smtplib.SMTP_SSL("smtp.qq.com", 465)  # 发件人邮箱中的SMTP服务器，端口是465
+        server.login(EMAIL_HOST_USER, EMAIL_HOST_PASSWORD)  # 括号中对应的是发件人邮箱账号、邮箱密码
+        server.sendmail(EMAIL_HOST_USER, [to,], msg.as_string())  # 括号中对应的是发件人邮箱账号、收件人邮箱账号、发送邮件
+        server.quit()
+        return True
+    except:
+        return False
+
+
 
 
 def send_email_confirm(email, code):
